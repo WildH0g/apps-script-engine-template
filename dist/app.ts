eefd143ab5347f4bddc173d6cd42408c1705d5aa
WildH0g <dmitry.kostyuk@gmail.com>
2023-06-21 15:01:07 +0200

    Add context detection for editor apps

--- dist/app.ts
+++ dist/app.ts
@@ -1,7 +1,39 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+function getContext(): {
+  gasClass:
+    | typeof DocumentApp
+    | typeof FormApp
+    | typeof SlidesApp
+    | typeof SpreadsheetApp;
+  name: 'DocumentApp' | 'FormApp' | 'SlidesApp' | 'SpreadsheetApp';
+  ui: GoogleAppsScript.Base.Ui;
+} {
+  const contexts = [
+    { gasClass: DocumentApp, name: 'DocumentApp' },
+    { gasClass: FormApp, name: 'FormApp' },
+    { gasClass: SlidesApp, name: 'SlidesApp' },
+    { gasClass: SpreadsheetApp, name: 'SpreadsheetApp' },
+  ];
+  let ui = null;
+  let context = null;
+  contexts.forEach((_context) => {
+    if (null !== ui) return;
+    try {
+      ui = _context.gasClass.getUi();
+      context = _context;
+      context.ui = ui;
+      console.log(`Selected context: ${_context.name}`);
+    } catch (err) {
+      ui = null;
+      context = null;
+    }
+  });
+  return context;
+}
+
 function onOpen() {
-  SpreadsheetApp.getUi()
-    .createMenu('👨‍🏭 Wurkspaces.dev')
+  const ui = getContext().ui;
+  ui.createMenu('👨‍🏭 Wurkspaces.dev')
     .addItem('☕🍽️ Boilerplate', 'showSidebar')
     .addToUi();
 }
