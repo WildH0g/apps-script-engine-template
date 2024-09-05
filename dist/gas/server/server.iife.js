1eff92805d32df9d3e79245c6b1c7b2cdb7680b9
WildH0g <dmitry.kostyuk@gmail.com>
2024-09-05 08:49:50 +0200

    Update build

--- dist/gas/server/server.iife.js
+++ dist/gas/server/server.iife.js
@@ -24,7 +24,8 @@ var lib_ = function(exports) {
     let ui = null;
     let context = null;
     contexts.forEach((_context) => {
-      if (null !== ui) return;
+      if (null !== ui)
+        return;
       try {
         ui = _context.gasClass.getUi();
         context = _context;
